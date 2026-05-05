f67659bbebfef73687acb5224a91d660cc8cdbb9
Egor Skorik <e.skorik@chillgaming.net>
2026-05-05 19:23:49 +0400

    add attibutes for codegen

---
+++ Src/Feature/Attributes.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace FFS.Libraries.StaticEcs {
+// required for codegen
+    
+// Component types
+public class TagAttribute : Attribute { }
+public class CmpAttribute : Attribute { }
+public class MulAttribute : Attribute { }
+public class EvtAttribute : Attribute { }
+
+public class FeatureAttribute : Attribute { }
+
+// System types
+public class PreAttribute : Attribute { }
+public class UpdAttribute : Attribute { }
+public class LatAttribute : Attribute { }
+public class FixAttribute : Attribute { }
+public class DrwAttribute : Attribute { }
+public class ClnAttribute : Attribute { }
+
+}
